0d5340b56d7b80e792c5557e3bac6eeda7c73892
Jacobo Blanco <jayb88@gmail.com>
2014-04-16 16:23:30 +0100

    Add logging and recent email list

--- Networking/Mail.rb
+++ Networking/Mail.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/ruby
 
+require 'net/imap'
+
 class Account
 
-  @@accounts = []
+  @@accounts = {}
 
   attr_reader :name, :address, :username, :password
 
@@ -11,7 +13,7 @@ class Account
     @address  = address
     @username = username
     @password = password
-    @@accounts << self
+    @@accounts[name] = self
   end
 
   def self.accounts
@@ -23,23 +25,45 @@ class Account
     puts "==================="
     @@accounts.map { |e| puts "* #{e.name}"  }
   end
+
+  def to_s
+    "Name: #{name}, Address: #{address}"
+  end
 end
 
 class Email
 
   def initialize(accounts)
     @accounts = accounts
-    Accounts.list_accounts
   end
 
-  def list_email(account)
-    imap = Net::IMAP.new('')
-    imap.authenticate()
+  def list_email(account_name)
+    
+    # Load account
+    account   = @accounts[account_name]
+
+    # Get account information
+    username  = account.username
+    password  = account.password
+    address   = account.address
+
+    # Load emails
+    puts "Loading emails from #{address}"
+    imap = Net::IMAP.new(address)
+    imap.authenticate('LOGIN', username, password)
+    
+    imap.examine('INBOX')
+    imap.search(["RECENT"]).each do |message_id|
+      envelope = imap.fetch(message_id, "ENVELOPE")[0].attr["ENVELOPE"]
+      puts "#{envelope.from[0].name}: \t#{envelope}"
+    end
   end
 end
 
 if __FILE__ == $0
-  Account.new('gmail', 'office365.outlook.com', 'zrap013', 'Jacobla88')
-  Account.new('rhul', 'office365.outlook.com', 'zrap013', 'Jacobla88')
+  Account.new('rhul', 'outlook.office365.com', 'zrap013@live.rhul.ac.uk', 'Jacobla88')
+  
   client = Email.new(Account.accounts)
+  
+  client.list_email('rhul')
 end
