b1c10aba8e30be8ecd8b5d352433ec5548c1f59a
Jacobo Blanco <jayb88@gmail.com>
2014-04-16 15:26:08 +0100

    Added account class

--- Networking/Mail.rb
+++ Networking/Mail.rb
@@ -1,4 +1,45 @@
 #!/usr/bin/ruby
 
-imap = Net::IMAP.new('')
-imap.authenticate()
+class Account
+
+  @@accounts = []
+
+  attr_reader :name, :address, :username, :password
+
+  def initialize(name, address, username, password)
+    @name     = name
+    @address  = address
+    @username = username
+    @password = password
+    @@accounts << self
+  end
+
+  def self.accounts
+    @@accounts
+  end
+
+  def self.list_accounts
+    puts "Registered accounts"
+    puts "==================="
+    @@accounts.map { |e| puts "* #{e.name}"  }
+  end
+end
+
+class Email
+
+  def initialize(accounts)
+    @accounts = accounts
+    @accounts.list_accounts
+  end
+
+  def list_email(account)
+    imap = Net::IMAP.new('')
+    imap.authenticate()
+  end
+end
+
+if __FILE__ == $0
+  Account.new('gmail', 'office365.outlook.com', 'zrap013', 'Jacobla88')
+  Account.new('rhul', 'office365.outlook.com', 'zrap013', 'Jacobla88')
+  client = Email.new(Account.accounts)
+end
